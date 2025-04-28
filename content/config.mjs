f01b595fb410c8dee5c82a571a097d4ef4fcc366
kevinzepeda <megakevizaat@gmail.com>
2025-04-28 13:43:22 -0600

    feat: content demo

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,26 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+const config = {
+  title: "My Awesome Blog",
+  description: "This is my awesome blog built with Flowershow",
+  author: "John",
+  authorLogo: "/images/logo.svg",
+  domain: "https://john.app/",
+  navLinks: [{ href: "/about", name: "About" }],
+  showSidebar: true,
+  social: [
+  {
+    href: "#",
+    name: "GitHub Profile",
+    label: "github"
+  },
+  {
+    href: "#",
+    name: "Follow me on X",
+    label: "twitter"
+  }
+],
+ };
+
+ export default config;
